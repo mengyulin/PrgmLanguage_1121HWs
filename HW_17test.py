f3c978b8d310a9a2160b0c0fae21b9740559f4db
Meng-Yu Lin <mylin@cycu.edu.tw>
2023-12-04 20:33:10 +0800

    HW_19

---
+++ HW_17test.py
@@ -0,0 +1,5 @@
+members = (('Jam', 16), ('Sam', 21), ('Mark', 17), ('Ken', 24), ('Ben', 18),\
+    ('Mary', 20), ('Ted', 19), ('Tom', 22), ('John', 23), ('Kevin', 25))
+
+for name, age in members:
+    print(name, age)
